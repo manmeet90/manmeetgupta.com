3b13c2482c63e532aabe8deea4f21aa6f53e9b4a
XOR-IND\gupta_m <mgupta@cloudvelox.com>
2018-01-19 12:40:14 +0530

    site updated

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const reactDOM = require("react-dom") ;
 const React = require("react");
-
 class App extends React.Component {
     render(){
         return(
             <div>
-                Hello World!
+                <section id="about">
+                    <h1>Manmeet Gupta</h1>
+                    <h2>Frontend Engineer, <span className="manu_text">Manchester United</span> Fan, Foodie :)</h2>
+                </section>
+                <footer>&copy; 2018 Manmeet Gupta</footer>
             </div>
         );
     }
 }
-
-reactDOM.render(<App />, document.getElementById("main"));
+reactDOM.render(<App />, document.querySelector("#main"));
