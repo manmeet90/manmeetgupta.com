6b31e9e8343196a1aa398818c715ac721cca72d4
XOR-IND\gupta_m <mgupta@cloudvelox.com>
2018-02-08 14:05:38 +0530

    weather widget updated

--- bundle.js
+++ bundle.js
@@ -9539,9 +9539,9 @@ class App extends React.Component {
                 React.createElement("section", {id: "weather_widget"}, 
                     React.createElement("div", null, 
                         React.createElement("small", {hidden: this.state.wd != null && this.state.isSecured}, "Finding weather information..."), 
-                        this.state.wd !=null && weatherInfo
+                        this.state.wd && weatherInfo
                     ), 
-                    React.createElement("div", {hidden: !this.state.wd && this.state.wda == false}, "Your browser doesn't support Geolocation APIs"), 
+                    React.createElement("div", {hidden: this.state.wd && this.state.wda == false}, "Your browser doesn't support Geolocation APIs"), 
                     React.createElement("div", {hidden: !this.state.isSecured}, "Visit to ", React.createElement("a", {href: "https://manmeetgupta.com/"}, "https version of site"), " to view weather information")
                 ), 
                 React.createElement("footer", null, "© 2018 Manmeet Gupta")
